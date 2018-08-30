c55a7ebce44980b755b5ae9cb218c19e8b25f8fb
Jhon Vryle Paulate <79091@corp.b2be.com>
2018-08-30 17:55:21 +0800

    10) Require own files

---
+++ n-3-3-require-own-files/app.js
@@ -0,0 +1,11 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
+
+console.log('Result:', notes.add(9, -2));
+console.log('addNote:', notes.addNote());
+// var user = os.userInfo();
+//
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
